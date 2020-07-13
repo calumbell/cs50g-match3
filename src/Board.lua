d39d72fd68d9912516576fb7234e3f66a73b9ee9
Calum Bell <calumabell@googlemail.com>
2020-07-13 18:55:50 +0100

    board now initialises with only flat blocks

--- src/Board.lua
+++ src/Board.lua
@@ -32,7 +32,7 @@ function Board:initializeTiles()
         for tileX = 1, 8 do
             
             -- create a new tile at X,Y with a random color and variety
-            table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), math.random(6)))
+            table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), 1))
         end
     end
 
