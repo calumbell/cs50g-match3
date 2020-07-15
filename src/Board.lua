a586a1ccd6bfb9ad520809759c8fc7033a605c23
Calum Bell <calumabell@googlemail.com>
2020-07-15 09:43:43 +0100

    boards at lvl1 are made up of all flat tiles, later lvls aren't

--- src/Board.lua
+++ src/Board.lua
@@ -13,10 +13,11 @@
 
 Board = Class{}
 
-function Board:init(x, y)
+function Board:init(x, y, level)
     self.x = x
     self.y = y
     self.matches = {}
+    self.level = level
 
     self:initializeTiles()
 end
@@ -31,8 +32,13 @@ function Board:initializeTiles()
 
         for tileX = 1, 8 do
             
-            -- create a new tile at X,Y with a random color and variety
-            table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), 1))
+            if self.level == 1 then
+                -- if first level, all tiles are flat (variety 1)
+                table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), 1))
+            else
+                -- else, randomise tile variety (1-6)
+                table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), math.random(6)))
+            end
         end
     end
 
--- src/states/BeginGameState.lua
+++ src/states/BeginGameState.lua
@@ -19,8 +19,7 @@ function BeginGameState:init()
     -- start our transition alpha at full, so we fade in
     self.transitionAlpha = 255
 
-    -- spawn a board and place it toward the right
-    self.board = Board(VIRTUAL_WIDTH - 272, 16)
+
 
     -- start our level # label off-screen
     self.levelLabelY = -64
@@ -30,6 +29,9 @@ function BeginGameState:enter(def)
     
     -- grab level # from the def we're passed
     self.level = def.level
+
+    -- spawn a board and place it toward the right
+    self.board = Board(VIRTUAL_WIDTH - 272, 16, self.level)
 
     --
     -- animate our white screen fade-in, then animate a drop-down with
